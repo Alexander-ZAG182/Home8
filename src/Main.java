1058b3188e7b58295c344d84bf9f6763ab553c16
Alexander-ZAG182 <hado18190@gmail.com>
2024-11-20 23:13:39 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,23 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Задача 1");
+        int[] numbers = new int[]{1,2,3};
+        numbers[0]=numbers[1];
+        for (int i = 0; i < numbers.length; i++)
+        { System.out.println(numbers[i]);}
 
+        }
 
+            
 
 
 
 
 
     }
-}
+
+
+
+
+
