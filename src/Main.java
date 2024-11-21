da8f1d1a2c70506c4c8a76398de4e93f048d2096
Alexander-ZAG182 <hado18190@gmail.com>
2024-11-21 23:05:24 +0300

    finish

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
@@ -28,10 +30,34 @@ public class Main {
         System.out.println();
         System.out.println("Задача 3");
         for (int i = numbers.length - 1; i >= 0; i--) {
-            System.out.print(numbers[i] + ",");
+            System.out.print(numbers[i]);
+            if (i != numbers.length - 3) {
+                System.out.print(",");
+            }
         }
-
-
+        System.out.println();
+        for (int i = numbers2.length - 1; i >= 0; i--) {
+            System.out.print(numbers2[i]);
+            if (i != numbers2.length - 3) {
+                System.out.print(",");
+            }
+        }
+        System.out.println();
+        for (int i = numbers3.length - 1; i >= 0; i--) {
+            System.out.print(numbers3[i]);
+            if (i != numbers3.length - 4) {
+                System.out.print(",");
+            }
+        }
+        System.out.println();
+        System.out.println("Задача 4");
+        int[] num = {1, 2, 3};
+        for (int i = 0; i < num.length; i++) {
+            if (num[i] % 2 != 0) {
+                num[i] = num[i] + 1;
+            }
+        }
+        System.out.print(Arrays.toString(num));
     }
 }
 
